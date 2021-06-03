d085f60aa2691341980c259df85c4807664ce758
yamini-priyaa <84720959+yamini-priyaa@users.noreply.github.com>
2021-06-03 20:47:40 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,13 +23,13 @@ int main(void) {
     }
   }
 }
-  float wallis_pi(int n){
-    int i;
-    float k,ans=1.0;
-    for(i=1;i<=n;i++){
-      k=(float)(4*i*i)/(4*i*i-1);
-      ans=ans*k;
-    }
-    return(2*ans);
+float wallis_pi(int n){
+  int i;
+  float k,ans=1.0;
+  for(i=1;i<=n;i++){
+    k=(float)(4*i*i)/(4*i*i-1);
+    ans=ans*k;
   }
+  return(2*ans);
+}
 
